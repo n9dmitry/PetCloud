63fb385850798b23ef4fa93f3f1f33ebc582d61b
n9dmitry <121239886+n9dmitry@users.noreply.github.com>
2023-03-04 21:50:11 +0300

    0.02 project

--- petcloud/petcloud/settings.py
+++ petcloud/petcloud/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'registr.apps.RegistrConfig',
+
 ]
 
 MIDDLEWARE = [
@@ -74,10 +76,14 @@ WSGI_APPLICATION = 'petcloud.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+'default': {
+'ENGINE': 'django.db.backends.postgresql_psycopg2',
+'NAME': 'petcloud',
+'USER': 'postgres',
+'PASSWORD': 'Access777',
+'HOST': '127.0.0.1',
+'PORT': '5432'
+}
 }
 
 
