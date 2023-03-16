8fb152b33c46b4ede63599c51cfc2964b1a2202f
n9dmitry <121239886+n9dmitry@users.noreply.github.com>
2023-03-16 13:54:41 +0300

    0.06

--- petcloud/petcloud/settings.py
+++ petcloud/petcloud/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'registr.apps.RegistrConfig',
     'news.apps.NewsConfig',
+    'oblako.apps.OblakoConfig',
 
 ]
 
--- petcloud/petcloud/urls.py
+++ petcloud/petcloud/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from news.views import *
 from registr.views import *
+from oblako.views import *
 
 
 urlpatterns = [
@@ -9,4 +10,6 @@ urlpatterns = [
     path('registr/', registr),
     path('editprofile/', editprofile, name='editprofile'),
     path('news_form/', news_add),
+    path('files_add/', views.entry, name='filesadd')
+
 ]
