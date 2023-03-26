eddde59b4b8e6140855af369d11bbe5ff82e8421
Nail Fatkhutdinov <liserg008@gmail.com>
2023-03-26 23:12:29 +0300

    Фронт

--- petcloud/registr/urls.py
+++ petcloud/registr/urls.py
@@ -9,8 +9,6 @@ from registr.views import *
 
 urlpatterns = [
 path('<int:pk>/', ProfileView.as_view(), name='profile_view'),
-path('login/', auth_views.LoginView.as_view(template_name='registr/login.html'), name = 'login'),
-path('logout/', auth_views.LogoutView.as_view(template_name='registr/logout.html')),
 path('profile_update/', ProfileUpdateView.as_view(), name='profile_update'),
 path('registr/', SignUp.as_view(), name='signup'),
 ]
